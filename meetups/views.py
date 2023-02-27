7193cd5b6e80fa33f9d0e6421f943a0df4310fee
elshater <elshater.ehab@gmail.com>
2023-02-27 18:35:55 +0200

    09-displaying meetups in index.html

--- meetups/views.py
+++ meetups/views.py
@@ -6,10 +6,22 @@ from django.http import HttpResponse
 
 def index(request):
     meetups=[
-        {'title':'first Meetup',},
-        {'title':'second Meetup',},
-        {'title':'third Meetup',},
-        ]
+        {
+            'title':'first Meetup', 
+            'location':'New York',
+            'slug':'a-first-meetup'
+        },
+        {
+            'title':'second Meetup',
+            'location':'Paris',
+            'slug':'a-second-meetup'
+        },
+        {
+            'title':'third Meetup',
+            'location':'London',
+            'slug':'a-third-meetup'
+        },
+    ]
     return render(request,'meetups/index.html',{
         'show_meetups':True,
         'meetups': meetups
